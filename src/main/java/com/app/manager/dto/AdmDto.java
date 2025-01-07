939236b2e8fd6be91b09d4b7b26724eb5c3fbcd1
NicNias <ananiasdias0@gmail.com>
2025-01-07 10:22:38 -0300

    fix: DTO adm

--- src/main/java/com/app/manager/dto/AdmDto.java
+++ src/main/java/com/app/manager/dto/AdmDto.java
@@ -3,7 +3,10 @@ package com.app.manager.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 public record AdmDto(
+        @NotBlank @NotNull UUID id,
         @NotBlank @NotNull String name,
         @NotBlank @NotNull String email,
         @NotBlank @NotNull String password
